bcaa01ccccd4d305491ad0713f6af91847a258fe
YoanDieu <yoan.dieu@gmail.com>
2015-12-20 20:06:20 +0100

    added victory check and reset grid fonctions

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -41,17 +41,113 @@ var game_started = false;
 var game_ended = true;
 var count = 1;
 var current_player;
-
+var winner = 0;
 
 
 //fonctions //
 
+function check_rows(){
+  if ($col_1.hasChildNodes() && $col_2.hasChildNodes() && $col_3.hasChildNodes()){
+    if ($col_1.firstChild.className == $col_2.firstChild.className && $col_2.firstChild.className == $col_3.firstChild.className){
+      winner = current_player;
+      $col_1.style.backgroundColor = "lightgreen";
+      $col_2.style.backgroundColor = "lightgreen";
+      $col_3.style.backgroundColor = "lightgreen";
+    }
+  }
+  if ($col_4.hasChildNodes() && $col_5.hasChildNodes() && $col_6.hasChildNodes()){
+    if ($col_4.firstChild.className == $col_5.firstChild.className && $col_5.firstChild.className == $col_6.firstChild.className){
+      winner = current_player;
+      $col_4.style.backgroundColor = "lightgreen";
+      $col_5.style.backgroundColor = "lightgreen";
+      $col_6.style.backgroundColor = "lightgreen";
+    }
+  }
+  if ($col_7.hasChildNodes() && $col_8.hasChildNodes() && $col_9.hasChildNodes()){
+    if ($col_7.firstChild.className == $col_8.firstChild.className && $col_8.firstChild.className == $col_9.firstChild.className){
+      winner = current_player;
+      $col_7.style.backgroundColor = "lightgreen";
+      $col_8.style.backgroundColor = "lightgreen";
+      $col_9.style.backgroundColor = "lightgreen";
+    }
+  }
+}
+
+function check_cols(){
+  if ($col_1.hasChildNodes() && $col_4.hasChildNodes() && $col_7.hasChildNodes()){
+    if ($col_1.firstChild.className == $col_4.firstChild.className && $col_4.firstChild.className == $col_7.firstChild.className){
+      winner = current_player;
+      $col_1.style.backgroundColor = "lightgreen";
+      $col_4.style.backgroundColor = "lightgreen";
+      $col_7.style.backgroundColor = "lightgreen";
+    }
+  }
+  if ($col_2.hasChildNodes() && $col_5.hasChildNodes() && $col_8.hasChildNodes()){
+    if ($col_2.firstChild.className == $col_5.firstChild.className && $col_5.firstChild.className == $col_8.firstChild.className){
+      winner = current_player;
+      $col_2.style.backgroundColor = "lightgreen";
+      $col_5.style.backgroundColor = "lightgreen";
+      $col_8.style.backgroundColor = "lightgreen";
+    }
+  }
+  if ($col_3.hasChildNodes() && $col_6.hasChildNodes() && $col_9.hasChildNodes()){
+    if ($col_3.firstChild.className == $col_6.firstChild.className && $col_6.firstChild.className == $col_9.firstChild.className){
+      winner = current_player;
+      $col_3.style.backgroundColor = "lightgreen";
+      $col_6.style.backgroundColor = "lightgreen";
+      $col_9.style.backgroundColor = "lightgreen";
+    }
+  }
+}
+
+function check_diags(){
+  if ($col_1.hasChildNodes() && $col_5.hasChildNodes() && $col_9.hasChildNodes()){
+    if ($col_1.firstChild.className == $col_5.firstChild.className && $col_5.firstChild.className == $col_9.firstChild.className){
+      winner = current_player;
+      $col_1.style.backgroundColor = "lightgreen";
+      $col_5.style.backgroundColor = "lightgreen";
+      $col_9.style.backgroundColor = "lightgreen";
+    }
+  }
+  if ($col_3.hasChildNodes() && $col_5.hasChildNodes() && $col_7.hasChildNodes()){
+    if ($col_3.firstChild.className == $col_5.firstChild.className && $col_5.firstChild.className == $col_7.firstChild.className){
+      winner = current_player;
+      $col_3.style.backgroundColor = "lightgreen";
+      $col_5.style.backgroundColor = "lightgreen";
+      $col_7.style.backgroundColor = "lightgreen";
+    }
+  }
+}
+
+function reset_grid(){
+  console.log("strarting reset");
+  for(i=0; i < col_array.length; i++){
+    if(col_array[i].firstChild != null){
+      col_array[i].removeChild(col_array[i].firstChild);
+      col_array[i].style.backgroundColor = "lightgrey";
+    }
+
+  }
+
+}
+
 function add_a_cross(e){
-  e.target.appendChild($cross.cloneNode());
+
+  if(e.target.hasChildNodes() == false){
+    e.target.appendChild($cross.cloneNode());
+    count++;
+  }else{
+    $intel_board.innerHTML = "<h4> Cette case a déjà été jouée </h4>"
+  }
 }
 
 function add_a_cicle(e){
-  e.target.appendChild($circle.cloneNode());
+  if(e.target.hasChildNodes() == false){
+    e.target.appendChild($circle.cloneNode());
+    count++;
+  }else {
+    $intel_board.innerHTML = "<h4> Cette case a déjà été jouée </h4>"
+  }
 }
 
 function announce_player(){
@@ -66,9 +162,12 @@ function announce_player(){
 
 function announce_partie_ended(){
   $intel_board.innerHTML = "<h4> La partie est finie !! </h4>";
+  count = 1;
 }
 
 function start() {
+  reset_grid();
+  count = 1;
   game_started = true;
   game_ended = false;
   console.log('la partie est lancé !')
@@ -76,15 +175,20 @@ function start() {
   announce_player();
 }
 
-function play(e){
+function end(){
+  game_started = false;
+  game_ended = true;
+  count = 1;
+  announce_partie_ended()
+  console.log('la partie est finie!');
+}
 
+function play(e){
   if (game_started){
-    count++;
-    if(count > col_array.length + 1){
-      game_started = false;
-      game_ended = true;
-      announce_partie_ended()
-      console.log('la partie est finie!');
+
+    if(count > col_array.length - 1){
+        add_a_cross(e);
+        end();
       } else {
         // here you play //
         if(current_player == 1){
@@ -96,7 +200,11 @@ function play(e){
         announce_player();
         console.log(count);
       }
+      check_rows();
+      check_cols();
+      check_diags();
     }else{
+      $intel_board.innerHTML = "<h4> Aucune partie en cours, cliquez sur 'commencer' ! </h4>"
       console.log('Aucune partie en cours.');
     }
 
